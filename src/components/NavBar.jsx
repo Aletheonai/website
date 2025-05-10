715d77789f259a588bad94353616bc0ed99b8f49
agapius-boughosn <79974674+agapius-boughosn@users.noreply.github.com>
2025-05-10 13:30:27 +0200

    Floating navigation bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Navbar() {
-  const [mobileOpen, setMobileOpen] = useState(false);
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (typeof window !== 'undefined') {
+      if (window.scrollY > lastScrollY) {
+        // if scroll down hide the navbar
+        setShowNavbar(false);
+      } else {
+        // if scroll up show the navbar
+        setShowNavbar(true);
+      }
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlNavbar);
+
+      // cleanup function
+      return () => {
+        window.removeEventListener('scroll', controlNavbar);
+      };
+    }
+  }, [lastScrollY]);
+
   return (
-    <header className={`navbar ${mobileOpen ? 'open' : ''}`}>
-      <div className="logo">ALETHEON</div>
+    <header className={`navbar ${showNavbar ? 'active' : 'hidden'}`}>
+      <div className="logo">ΛLETHEΘN</div>
       <nav>
         <ul className="nav-links">
-          <li><a href="#hero">Home</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="#hero"><b>Home</b></a></li>
+          <li><a href="#services"><b>Services</b></a></li>
+          <li><a href="#contact"><b>Contact</b></a></li>
         </ul>
       </nav>
       <button 
